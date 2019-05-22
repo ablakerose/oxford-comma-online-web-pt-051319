3a5c6626f0b409f6d86ff6ddaebd6958a9b59989
Blake Rose <bdecktor@gmail.com>
2019-05-22 15:18:48 +0000

    Automatically backed up by Learn

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -1,8 +1,8 @@
 def oxford_comma(array)
-  if array.length == 2
-  array[-2] << " and "
-  array.join
-else
-  array.join
+  if array.length == 1
+    array.join 
+  else array.length == 2
+    array[-2] << " and "
+    array.join
 end
 end
